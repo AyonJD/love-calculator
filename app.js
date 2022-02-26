e663c3a0a4bbc541ac7620e9b5e7edba90c864eb
Ayon <ayonjodder177@gmail.com>
2022-02-26 23:30:43 +0600

    api key removed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ button.addEventListener('click', () => {
 			"method": "GET",
 			"headers": {
 				"x-rapidapi-host": "love-calculator.p.rapidapi.com",
-				"x-rapidapi-key": "fdab746e91msh396c32ae260aa5bp147ce3jsn1357eb1a5bad"
+				"x-rapidapi-key": "YOUR_API_KEY"
 			}
 		})
 			.then(res => res.json())
