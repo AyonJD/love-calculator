8bb287ba5ff2ffcdcc49fdd71cd55f9cb2e3e309
Ayon <ayonjodder177@gmail.com>
2022-02-26 16:42:19 +0600

    JS functionality added with API

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-
-fetch("https://love-calculator.p.rapidapi.com/getPercentage?sname=SmrityMondal&fname=AyonJodder", {
+//Getting Input value
+function getInput(inputId) {
+	const inputField = document.getElementById(inputId);
+	const inputValue = inputField.value;
+	return inputValue;
+}
+//Adding listener for button and then build the url based on input and fetch data from server
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+	const yourName = getInput('fname');
+	const partnerName = getInput('sname');
+	const url = `https://love-calculator.p.rapidapi.com/getPercentage?sname=${partnerName}&fname=${yourName}`
+	//Clear input Field
+	clear();
+	fetch(url, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "love-calculator.p.rapidapi.com",
@@ -7,4 +20,35 @@ fetch("https://love-calculator.p.rapidapi.com/getPercentage?sname=SmrityMondal&f
 	}
 })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => toDoData(data))
+})
+//Updating innerHtml based on data
+function toDoData(data) {
+	console.log(data)
+	const innerContent = document.getElementById('inner-content');
+	innerContent.innerHTML = `
+		<h1 class="py-3 text-lg font-bold">Your Name: <span class="text-teal-500">${data.fname}</span></h1>
+		<h1 class="py-3 text-lg font-bold">Your Partner Name: <span class="text-teal-500">${data.sname}</span></h1>
+		<h1 class="py-3 text-lg font-bold">Love Parcentage: <span class="text-teal-500">${data.percentage}%</span></h1>
+		<h1 class="py-3 text-lg font-bold">Your Next Step: <span class="text-teal-500">${data.result}</span></h1>
+	`
+}
+//clearing input Field
+const clear = () => {
+	const input = Array.from(document.querySelectorAll('input'));
+	input.forEach(e => {
+		e.value = ''
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
